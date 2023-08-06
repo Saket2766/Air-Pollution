4f497ae43349b831d17a3341d122834ae0f58ec8
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-08-07 00:08:47 +0530

    deleted:    frontend/src/App.css 	modified:   frontend/src/App.jsx 	modified:   frontend/src/components/Navbar.jsx 	new file:   frontend/src/styles/navbar.css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import {Link} from "react-router-dom"
-
+import "../styles/navbar.css"
 const Navbar = () => {
     return ( 
         <header>
-            <li>
-                <Link>Search</Link>
-            </li>
-            <li>
-                <Link>Projects</Link>
-            </li>
-            <li>
-                <Link>About Me</Link>
-            </li>
+            <div className="logo">
+                <h1>Logo</h1>
+            </div>
+            <ul>
+                <li>
+                    <Link>Search</Link>
+                </li>
+                <li>
+                    <Link>Projects</Link>
+                </li>
+                <li>
+                    <Link>About Me</Link>
+                </li>
+            </ul>
         </header>
      );
 }
