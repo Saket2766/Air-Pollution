d15dc0639543c6790f05a485093f216905245008
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-08-07 17:44:17 +0530

    new file:   frontend/public/chris-leboutillier-TUJud0AWAPI-unsplash 1.png 	new file:   frontend/src/components/Carousel.jsx 	modified:   frontend/src/index.css 	modified:   frontend/src/styles/navbar.css 	deleted:    frontend/public/chris-leboutillier-TUJud0AWAPI-unsplash 1.png 	frontend/public/Slide1.png

---
+++ frontend/src/components/Carousel.jsx
@@ -0,0 +1,11 @@
+const Carousel = () => {
+    return ( 
+        <div className="carousel">
+            <div className="slide">
+                <img src="/"/>
+            </div>
+        </div>
+     );
+}
+ 
+export default Carousel;
