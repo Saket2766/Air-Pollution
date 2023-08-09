12d0bd1a30c9e69cfc6b9ce6dc1e767905b82a41
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-08-09 18:14:48 +0530

    modified:   frontend/src/components/Carousel.jsx 	modified:   frontend/src/pages/Home.jsx 	modified:   frontend/src/styles/home.css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,9 @@ import "../styles/home.css"
 const Home = () => {
 
     const slides = [
-        {url:"/Slide1.png" ,title: "Air Pollution"}
+        {url:"/Slide1.png" ,title: "Air Pollution"},
+        {url:"/Slide1.png" ,title: "Water Pollution"},
+        {url:"/Slide1.png" ,title: "Land Pollution"}
     ]
 
     return (
