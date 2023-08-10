7a88e6156afdf455e8ea79d03151ed0ae82be910
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-08-10 11:33:41 +0530

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/App.jsx 	new file:   frontend/src/components/ChartLine.jsx 	modified:   frontend/src/styles/AboutProject.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,59 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import ChartDropdown from "./components/ChartDropdown";
+import ChartLine from "./components/ChartLine";
+
 
 const Options = [
-  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'chocolate', label: 'Chocolate'},
   { value: 'strawberry', label: 'Strawberry' },
   { value: 'vanilla', label: 'Vanilla' }
 ]
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100
+  }
+];
 
 function App() {
   return (
@@ -23,6 +70,7 @@ function App() {
         <ChartDropdown Options={Options}/>
         <ChartDropdown Options={Options}/>
         <ChartDropdown Options={Options}/>
+        <ChartLine data={data}/>
       </BrowserRouter>
     </div>
   );
