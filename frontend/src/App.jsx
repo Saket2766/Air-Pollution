f9111b39a071e58179fc185d8ef531696139c330
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-08-10 10:19:18 +0530

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/App.jsx 	new file:   frontend/src/components/ChartDropdown.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import ChartDropdown from "./components/ChartDropdown";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
         <Footer />
+        <ChartDropdown/>
       </BrowserRouter>
     </div>
   );
