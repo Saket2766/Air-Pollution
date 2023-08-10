89917e381346637de1752046452cd9cd1c95257a
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-08-10 10:29:56 +0530

    modified:   frontend/src/App.jsx 	modified:   frontend/src/components/ChartDropdown.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,12 @@ import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import ChartDropdown from "./components/ChartDropdown";
 
+const Options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+
 function App() {
   return (
     <div className="App">
@@ -14,7 +20,9 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
         <Footer />
-        <ChartDropdown/>
+        <ChartDropdown Options={Options}/>
+        <ChartDropdown Options={Options}/>
+        <ChartDropdown Options={Options}/>
       </BrowserRouter>
     </div>
   );
--- frontend/src/components/ChartDropdown.jsx
+++ frontend/src/components/ChartDropdown.jsx
@@ -2,12 +2,6 @@ import React, { useState } from 'react';
 
 import Select from 'react-select';
 
-const colourOptions = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ]
-
 const Checkbox = ({ children, ...props }) => (
   <label style={{ marginRight: '1em' }}>
     <input type="checkbox" {...props} />
@@ -15,7 +9,7 @@ const Checkbox = ({ children, ...props }) => (
   </label>
 );
 
-const ChartDropdown = () => {
+const ChartDropdown = ({Options}) => {
     const [isClearable, setIsClearable] = useState(true);
     const [isSearchable, setIsSearchable] = useState(true);
     const [isDisabled, setIsDisabled] = useState(false);
@@ -27,14 +21,14 @@ const ChartDropdown = () => {
         <Select
           className="basic-single"
           classNamePrefix="select"
-          defaultValue={colourOptions[0]}
+          defaultValue={Options[0]}
           isDisabled={isDisabled}
           isLoading={isLoading}
           isClearable={isClearable}
           isRtl={isRtl}
           isSearchable={isSearchable}
           name="color"
-          options={colourOptions}
+          options={Options}
         />
   
         <div
