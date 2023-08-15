d2ff2515f8e49cd2f5b0ce9c647e4d6d6fd61aec
Dhruv Chandak <dhruvchandak5@gmail.com>
2023-08-15 16:54:55 +0530

    Added Responsiveness

--- frontend/src/components/AboutMe.jsx
+++ frontend/src/components/AboutMe.jsx
@@ -3,13 +3,16 @@ import Anikeit from "../Images/AnikeitSaxena.jpeg";
 const AboutMe = () => {
   return (
     <div className="AboutMe">
-      <img src={Anikeit} alt="Picture of Anikeit Saxena"></img>
+      <h1>About Me</h1>
       <div className="AboutMeContent">
-        <h1>About Me</h1>
+        <img src={Anikeit} alt="Picture of Anikeit Saxena"></img>
         <p>
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic mollitia modi quibusdam assumenda corporis, quas enim ratione asperiores eveniet quisquam, molestias eius. Temporibus deleniti ipsam, nulla enim voluptatum eos quae.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic mollitia
+          modi quibusdam assumenda corporis, quas enim ratione asperiores
+          eveniet quisquam, molestias eius. Temporibus deleniti ipsam, nulla
+          enim voluptatum eos quae.
         </p>
-        <a href="">Learn More</a>
+        <a href="/">Learn More</a>
       </div>
     </div>
   );
--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,63 +1,69 @@
 import { useState } from "react";
 
-const Carousel = ({slides}) => {
+const Carousel = ({ slides }) => {
+  const [slideIndex, setSlideIndex] = useState(0);
 
-    const [slideIndex,setSlideIndex] = useState(0);
+  const updateDot = () => {
+    var dots = $(".dot");
+    dots.map((dot, idx) => {
+      if (idx === slideIndex) {
+        dot.addClass("active");
+        dot.removeClass("dot");
+      } else {
+        dot.addClass("dot");
+        dot.removeClass("active");
+      }
+    });
+  };
 
-    const updateDot = () => {
-        var dots = $('.dot');
-        dots.map ( (dot,idx) => {
-            if(idx === slideIndex){
-                dot.addClass('active');
-                dot.removeClass('dot');
-            }
-            else{
-                dot.addClass('dot');
-                dot.removeClass('active');
-            }
-        })
-    } 
-
-    const nextSlide = () =>{
-        if(slides.length === slideIndex+1){
-            setSlideIndex(0);
-        }else{
-            setSlideIndex(slideIndex+1);
-        }
-        updateDot();
+  const nextSlide = () => {
+    if (slides.length === slideIndex + 1) {
+      setSlideIndex(0);
+    } else {
+      setSlideIndex(slideIndex + 1);
     }
-    const prevSlide = () =>{
-        if(slideIndex-1 < 0){
-            setSlideIndex(slides.length-1);
-        }
-        else{
-            setSlideIndex(slideIndex-1);
-        }
-        updateDot();
+    updateDot();
+  };
+  const prevSlide = () => {
+    if (slideIndex - 1 < 0) {
+      setSlideIndex(slides.length - 1);
+    } else {
+      setSlideIndex(slideIndex - 1);
     }
+    updateDot();
+  };
 
-    const jumpToSilde = (index) => {
-        setSlideIndex(index);
-        updateDot();
-    }
+  const jumpToSilde = (index) => {
+    setSlideIndex(index);
+    updateDot();
+  };
 
-    return ( 
-        <div className="carousel" style={{backgroundImage : `url(${slides[slideIndex].url})`}}>
-            <div className="slide">
-                <div className="arrow" onClick={prevSlide}><img src="/ArrowLeft.png" alt="left arrow" /></div>
-                <div className="title-container"> 
-                    <h1 className="slide-title">{slides[slideIndex].title}</h1>
-                    <button className="title-button">LEARN MORE</button>
-                </div>
-                <div className="arrow" onClick={nextSlide}><img src="/ArrowRight.png" alt="right arrow" /></div>
-                <div className="dot-container">
-                    {slides.map( (slide,idx) => (
-                        <div key={idx} className="dot" onClick={() => jumpToSilde(idx)}>&#9679;</div>
-                    ) )}
-                </div>
-            </div>
+  return (
+    <div
+      className="carousel"
+      style={{ backgroundImage: `url(${slides[slideIndex].url})` }}
+    >
+      <div className="slide">
+        <div className="arrow" onClick={prevSlide}>
+          <img src="/ArrowLeft.png" alt="left arrow" />
         </div>
-     );
-}
- 
+        <div className="title-container">
+          <h1 className="slide-title">{slides[slideIndex].title}</h1>
+          <button className="title-button">LEARN MORE</button>
+        </div>
+        <div className="arrow" onClick={nextSlide}>
+          <img src="/ArrowRight.png" alt="right arrow" />
+        </div>
+        <div className="dot-container">
+          {slides.map((slide, idx) => (
+            <div key={idx} className="dot" onClick={() => jumpToSilde(idx)}>
+              &#9679;
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Carousel;
