c1d26d1fc96efc7966f79476ec27eebc3d7cfa2e
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-08-19 17:01:35 +0530

    modified:   frontend/src/components/AllCharts.jsx

--- frontend/src/components/AllCharts.jsx
+++ frontend/src/components/AllCharts.jsx
@@ -2,7 +2,7 @@ import DropDownMenu from "../components/DropDownMenu";
 
 const AllCharts = ({charts}) => {
   return (
-    <div>
+    <div style={{flexGrow:1}}>
       <h1>All Charts</h1>
       {charts}
     </div>
