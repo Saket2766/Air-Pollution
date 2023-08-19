27afefabaee902fcf3f6505f100ec423f5a826cf
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-08-19 18:39:50 +0530

    new file:   frontend/public/hamburger.svg 	modified:   frontend/src/components/Sidebar.jsx 	modified:   frontend/src/styles/Sidebar.css

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,14 +1,25 @@
+import { useRef, useState } from "react";
 import "../styles/Sidebar.css";
 
 const Sidebar = ({showStatewise,showYearwise,showCitywiseForState,showCitywise}) => {
+
+    const [active,setActive] = useState(false);
+
+    const toggleHidden = () => {
+        setActive(!active);
+    }
+
   return (
-    <nav className="sidebar">
-      <div className="sidebar-label">Visualizations</div>
-      <div className="sidebar-item" onClick={showYearwise}>Year Wise Comparison</div>
-      <div className="sidebar-item" onClick={showStatewise}>Statewise Comparison</div>
-      <div className="sidebar-item" onClick={showCitywiseForState}>Citywise Comparison for Each State</div>
-      <div className="sidebar-item" onClick={showCitywise}>City wise Comparison</div>
-    </nav>
+    <>
+        <div className="hamburger"  onClick={toggleHidden}><img src="/hamburger.svg" alt="ham"/></div>
+        <nav className={`sidebar ${active? "":"hidden"}`}>
+            <div className="sidebar-label">Visualizations</div>
+            <div className="sidebar-item" onClick={showYearwise}>Year Wise Comparison</div>
+            <div className="sidebar-item" onClick={showStatewise}>Statewise Comparison</div>
+            <div className="sidebar-item" onClick={showCitywiseForState}>Citywise Comparison for Each State</div>
+            <div className="sidebar-item" onClick={showCitywise}>City wise Comparison</div>
+        </nav>
+    </> 
   );
 };
 
