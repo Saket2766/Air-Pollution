d666b86ba33140a892a0b519b0474d21c8965b65
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-08-19 14:32:26 +0530

    modified:   frontend/src/components/Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,11 +1,11 @@
 import "../styles/Sidebar.css";
 
-const Sidebar = () => {
+const Sidebar = ({showStatewise}) => {
   return (
     <nav className="sidebar">
       <div className="sidebar-label">Visualizations</div>
       <div className="sidebar-item">Year Wise Comparison</div>
-      <div className="sidebar-item">Statewise Comparison</div>
+      <div className="sidebar-item" onClick={showStatewise}>Statewise Comparison</div>
       <div className="sidebar-item">Citywise Comparison for Each State</div>
       <div className="sidebar-item">City wise Comparison</div>
     </nav>
