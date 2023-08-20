f64aca2c5051ac6af76ba0016845a4f624c67c31
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-08-20 13:07:51 +0530

    modified:   frontend/src/components/BarChart/Viz2.jsx 	modified:   frontend/src/styles/Sidebar.css 	modified:   frontend/src/styles/VizStyles.css

--- frontend/src/components/BarChart/Viz2.jsx
+++ frontend/src/components/BarChart/Viz2.jsx
@@ -14,13 +14,13 @@ export default function Viz2(prop) {
   const data = prop.data;
   return (
     <div className="chart">
-      <ResponsiveContainer width="210%" height="160%">
+      <ResponsiveContainer width="100%" height="100%">
         <BarChart
           width={500}
           height={300}
           data={data}
           margin={{
-            top: 105,
+            top: 5,
             right: 30,
             left: 20,
             bottom: 5,
